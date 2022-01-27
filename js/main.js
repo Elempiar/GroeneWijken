2774afe909f3242ac624c179327fec1f288a2c53
Luuk Verboeket <Lverboeket@hotmail.com>
2022-01-27 15:32:39 +0100

    Site Opening
    Added an opening Overlay for the site

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+// Overlay //
+
+const TEXT_TITEL = "Groene Wijken";
+
+$(document).ready(function () {
+    $("#intro-video").hide();
+    $("#intro-text").hide();
+    $("#btnContinue").hide();
+    $("<h1>" + TEXT_TITEL + "</h1>").hide().prependTo("#overlay").fadeIn(2000).fadeOut(3000);
+    showVideo();
+
+    function showVideo() {
+        setTimeout(function () {
+            $("#intro-video").fadeIn(2000);
+            $("#btnContinue").fadeIn(2000);
+        }, 4500);
+    }
+
+    function showText() {
+        setTimeout(function () {
+            $("#intro-text").fadeIn(2000);
+        }, 1000);
+    }
+
+    $(".btn").click(function () {
+
+        const btnID = $(this).attr("id");
+
+        if (btnID == "btnContinue") {
+            $(this).prop("id", "btnOverlay").html("verken de wijk");
+            $("#intro-video").fadeOut(1000);
+            showText();
+        } else if (btnID == "btnOverlay") {
+            $("#overlay").css("top", "-100vh");
+            scrollTo(documentCenter, 0);
+        }
+    });
+
+});
+
+
 // SCROLL NAV
 
 const FRAME_DURATION = 1000 / 60; // 60fps frame duration
@@ -8,8 +49,6 @@ const documentCenter = ($(document).width() - $(window).width()) / 2;
 const SCROLL_SPEED = 10;
 
 $(document).ready(function () {
-
-    scrollTo(documentCenter, 0);
 
     let isScrolling = false;
     let scrollDirection = null;
@@ -52,10 +91,6 @@ $(document).ready(function () {
     });
 });
 
-$("#btnScroll").click(function () {
-    scrollTo(documentCenter, 0);
-});
-
 
 // HOVER EFFECT
 
@@ -68,27 +103,26 @@ const TEXT_HOVER_7 = "Geothermie";
 
 $(".hover").hover(function () {
 
-    const hoverDiv = $(this).attr("id");
+    const hoverID = $(this).attr("id");
 
     $(this).css("filter", "grayscale(0)");
 
-    if (hoverDiv == "c1") {
-        $("<h1>" + TEXT_HOVER_1 + "</h1>").hide().prependTo(".container").fadeIn(500);
-    } else if (hoverDiv == "c2") {
-        $("<h1>" + TEXT_HOVER_2 + "</h1>").hide().prependTo(".container").fadeIn(500);
-    } else if (hoverDiv == "c3") {
-        $("<h1>" + TEXT_HOVER_3 + "</h1>").hide().prependTo(".container").fadeIn(500);
-    } else if (hoverDiv == "c5") {
-        $("<h1>" + TEXT_HOVER_5 + "</h1>").hide().prependTo(".container").fadeIn(500);
-    } else if (hoverDiv == "c6") {
-        $("<h1>" + TEXT_HOVER_6 + "</h1>").hide().prependTo(".container").fadeIn(500);
-    } else if (hoverDiv == "c7") {
-        $("<h1>" + TEXT_HOVER_7 + "</h1>").hide().prependTo(".container").fadeIn(500);
+    if (hoverID == "c1") {
+        $("<h2>" + TEXT_HOVER_1 + "</h2>").hide().prependTo(".map").fadeIn(500);
+    } else if (hoverID == "c2") {
+        $("<h2>" + TEXT_HOVER_2 + "</h2>").hide().prependTo(".map").fadeIn(500);
+    } else if (hoverID == "c3") {
+        $("<h2>" + TEXT_HOVER_3 + "</h2>").hide().prependTo(".map").fadeIn(500);
+    } else if (hoverID == "c5") {
+        $("<h2>" + TEXT_HOVER_5 + "</h2>").hide().prependTo(".map").fadeIn(500);
+    } else if (hoverID == "c6") {
+        $("<h2>" + TEXT_HOVER_6 + "</h2>").hide().prependTo(".map").fadeIn(500);
+    } else if (hoverID == "c7") {
+        $("<h2>" + TEXT_HOVER_7 + "</h2>").hide().prependTo(".map").fadeIn(500);
     }
 
-
 }, function () {
-    $("h1").fadeOut("500", function () {
+    $("h2").fadeOut("500", function () {
         $(this).remove();
     });
 });
