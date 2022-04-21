19356fdeb82d7ea96a27cb137009fe7882b4a08c
Luuk Verboeket <Lverboeket@hotmail.com>
2022-04-21 15:28:06 +0200

    fixes

--- js/main.js
+++ js/main.js
@@ -3,75 +3,81 @@
 const TEXT_TITEL = "Groene Wijken";
 
 $(document).ready(function () {
-    $("#intro-video").hide();
-    $("#intro-text").hide();
-    $("#btnContinue").hide();
-    $("<h1>" + TEXT_TITEL + "</h1>").hide().prependTo("#intro").fadeIn(2000).fadeOut(3000);
-    showVideo();
+  $("#intro-video").hide();
+  $("#intro-text").hide();
+  $("#btnContinue").hide();
+  $("<h1>" + TEXT_TITEL + "</h1>")
+    .hide()
+    .prependTo("#intro")
+    .fadeIn(2000)
+    .fadeOut(3000);
+  showVideo();
 
-    function showVideo() {
-        setTimeout(function () {
-            $("#intro-video").fadeIn(2000);
-            $("#btnContinue").fadeIn(2000);
-        }, 4500);
+  function showVideo() {
+    setTimeout(function () {
+      $("#intro-video").fadeIn(2000);
+      $("#btnContinue").fadeIn(2000);
+    }, 4500);
+  }
+
+  function showText() {
+    setTimeout(function () {
+      $("#intro-text").fadeIn(2000);
+    }, 1000);
+  }
+
+  function removeVideo() {
+    setTimeout(function () {
+      $("#intro-video").remove();
+    }, 1000);
+  }
+
+  function removeIntro() {
+    setTimeout(function () {
+      $("#intro").remove();
+    }, 1000);
+  }
+
+  $(".btn").click(function () {
+    const btnID = $(this).attr("id");
+
+    if (btnID == "btnContinue") {
+      $(this).prop("id", "btnOverlay");
+      $("#intro-video").fadeOut(1000);
+      showText();
+      removeVideo();
+    } else if (btnID == "btnOverlay") {
+      $("#intro").css("top", "-100vh");
+      $("#loader").css("z-index", "8");
+      removeIntro();
     }
-
-    function showText() {
-        setTimeout(function () {
-            $("#intro-text").fadeIn(2000);
-        }, 1000);
-    }
-
-    function removeVideo() {
-        setTimeout(function () {
-            $("#intro-video").remove();
-        }, 1000);
-    }
-
-    function removeIntro() {
-        setTimeout(function () {
-            $("#intro").remove();
-        }, 1000);
-    }
-
-    $(".btn").click(function () {
-
-        const btnID = $(this).attr("id");
-
-        if (btnID == "btnContinue") {
-            $(this).prop("id", "btnOverlay").html("verken de wijk");
-            $("#intro-video").fadeOut(1000);
-            showText();
-            removeVideo();
-        } else if (btnID == "btnOverlay") {
-            $("#intro").css("top", "-100vh");
-            $("#loader").css("z-index", "8");
-            removeIntro();
-        }
-    });
+  });
 });
-
 
 // LOADING SCREEN
 
 $.ajax({
-    url: '/template-parts/map.html',
-    beforeSend: function () {
-        $('body').prepend('<div id="loader"><h4>loading</h4></div>');
-        $('#loader').append('<div id="load1" class="loading foop">  </div> <div id="load2" class="loading foop">  </div> <div id="load3" class="loading foop">  </div>');
+  url: "/template-parts/map.html",
+  beforeSend: function () {
+    $("body").prepend('<div id="loader"><h4>loading</h4></div>');
+    $("#loader").append(
+      '<div id="load1" class="loading foop">  </div> <div id="load2" class="loading foop">  </div> <div id="load3" class="loading foop">  </div>'
+    );
+    setTimeout(function () {
+      $(".loading").removeClass("foop");
+    }, 2500);
+  },
+  success: function (data) {
+    $(data)
+      .find("#bg")
+      .on("load", function () {
+        $("body").prepend(data);
+        $(".loading").removeClass("Loop");
+        $(".loading").addClass("poof");
+        $("#loader").addClass("loaderOut");
         setTimeout(function () {
-            $('.loading').removeClass('foop');
-        }, 2500);
-    },
-    success: function (data) {
-        $(data).find('#bg').on('load', function () {
-            $('body').prepend(data);
-            $('.loading').removeClass('Loop');
-            $(".loading").addClass('poof');
-            $('#loader').addClass('loaderOut');
-            setTimeout(function () {
-                $('#loader').remove();
-            }, 2400);
-        });
-    },
+          $("#loader").remove();
+        }, 2400);
+      });
+  },
 });
